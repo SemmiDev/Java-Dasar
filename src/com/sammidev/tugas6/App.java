bf714b5f62f4399a21af79fdd1b387e7c48e2296
SemmiDev <sammidev4@gmail.com>
2020-10-30 15:07:41 +0700

    tugas 5

---
+++ src/com/sammidev/tugas6/App.java
@@ -0,0 +1,56 @@
+package com.sammidev.tugas6;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        soal1();
+        soal2();
+    }
+
+
+    static void soal1() {
+        Scanner scanner = new Scanner(System.in);
+        String teks1,teks2;
+        System.out.print("Masukkan teks 1 : ");
+        teks1 = scanner.nextLine();
+
+        System.out.print("Masukkan teks 1 : ");
+        teks2 = scanner.nextLine();
+
+        teks1 = teks1.replaceAll("\\s+","");
+        teks2 = teks2.replaceAll("\\s+","");
+
+        boolean status;
+        if (teks1.equalsIgnoreCase(teks2)) {
+            status = true;
+        }else {
+            status = false;
+        }
+
+        System.out.println(status);
+    }
+
+    static void soal2() {
+
+        String nilai;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai 10 anak : ");
+        nilai = scanner.nextLine();
+
+        for (int i = 0; i < 10; i++) {
+            String[] temp = nilai.split(" ");
+            int angka  = Integer.parseInt(temp[i]);
+
+            if (angka >= 85 ) {
+                System.out.print("A ");
+            }else if(angka <= 84 && angka >= 75) {
+                System.out.print("B ");
+            }else if(angka <= 74 && angka >= 60 ) {
+                System.out.print("C ");
+            }else if(angka >= 45 && angka <= 59) {}
+            else {}
+        }
+    }
+}
