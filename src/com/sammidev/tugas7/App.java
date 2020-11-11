d30e4746943616d297b968d265fc49cb96b3973d
SemmiDev <sammidev4@gmail.com>
2020-11-11 14:29:38 +0700

    refactoring

--- src/com/sammidev/tugas7/App.java
+++ src/com/sammidev/tugas7/App.java
@@ -1,148 +1,160 @@
+package com.sammidev.tugas7;
+
 import java.util.Scanner;
 
 public class App {
 	public static void main(String[] args) {
-		// soal1();
-		// soal2();
-		// soal3();
-
-		int angka1, angka2,angka3;
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Masukkan angka 1 : ");
-		angka1 = scanner.nextInt();	
-
-		System.out.print("Masukkan angka 2 : ");
-		angka2 = scanner.nextInt();
-
-		System.out.print("Masukkan angka 3 : ");
-		angka3 = scanner.nextInt();
-
-
-		soal4(angka1, angka2, angka3);
-
+		soal1();
+		soal2();
+		soal3();
+		soal4a();
+		soal4b();
 	}
 
-	static void soal1() {	
-		int jam;
-		Scanner scanner = new Scanner(System.in);
+	static void soal1() {
+		clearScreen();
+		final int UPAH_PER_JAM = 2000;
+		final int UPAH_PER_JAM_LEMBUR = 3000;
+		final int LIMIT = 48;
+		int jam,
+				jamLembur = 0,
+				gaji = 0,
+				gajiLembur = 0;
+
+
 		System.out.print("Masukkan total jam anda bekerja : ");
-		jam = scanner.nextInt();
+		jam = scanner();
 
-		final int upahPerJam = 2000;
-		final int upahPerJamLembur = 3000;
-		int limit = 48;
-		int jamLembur = 0;
-		int gaji = 0;
-		int gajiLembur = 0;
+		if (jam <= LIMIT) gaji = jam * UPAH_PER_JAM;
+		else
+			jamLembur = jam - LIMIT;
+		gajiLembur = jamLembur * UPAH_PER_JAM_LEMBUR;
+		gaji = gajiLembur + (jam - jamLembur) * UPAH_PER_JAM;
 
-		if (jam <= limit) {
-			gaji = jam * upahPerJam;
-		}else {
-			jamLembur = jam - limit;
-			gajiLembur = jamLembur * upahPerJamLembur; // 6000
-			gaji = gajiLembur +   (jam - jamLembur) * upahPerJam; // 6000 + 96000 = 102000
-		}
 
-		System.out.println("Jam kerja lembur anda  : " + jamLembur);
-		System.out.println("Bonus           : Rp." + gajiLembur);
-		System.out.println("Total Gaji anda : Rp." + gaji);
+		System.out.println("Jam kerja lembur anda : " + jamLembur + " jam\n" +
+				"Bonus hasil kerja lembur : Rp." + gajiLembur + "\n" +
+				"Total gaji 		      : Rp." + gaji);
 	}
 
 	static void soal2() {
+		clearScreen();
 		int suhu;
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Masukkan suhu : ");
+		System.out.print("Masukkan Suhu : ");
 		suhu = scanner.nextInt();
-		String status;
 
-		switch(suhu) {
-			case 30 : 
-				status = "Panas";
-				break;
-			case 24 :
-				status = "Normal";
-				break;
-			case 20 :
-				status = "Dingin";
-				break;
-			default : 
-				status = "STATUS SUHU TIDAK DIKETAHUI";
-		}
+		// switch with lambda style
+		String status = switch (suhu) {
+			case 30 -> "Panas";
+			case 24 -> "Normal";
+			case 20 -> "Dingin";
+			default -> "STATUS SUHU TIDAK DIKETAHUI";
+		};
 
-		System.out.println(status);
+		System.out.println("Suhu : " + suhu + "\n" +
+				"Status : " + status);
 	}
 
-
 	static void soal3() {
-		String name,golongan; // A B C
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Masukkan nama : ");
-		name = scanner.nextLine();
+		clearScreen();
+		String name = scanner("Masukkan Nama");
+		String golongan = scanner("Masukkan Golongan");
 
-		System.out.print("Masukkan golongan : ");
-		golongan = scanner.next();
-		
-		final double golonganA = 400;
-		final double golonganB = 500;
-		final double golonganC = 750;
-		final double golonganD = 900;
-		double gajiBersih = 0,pajak,tunjangan;
+		double gajiBersih = 0, pajak, tunjangan;
+		final double golonganA = 400,
+				golonganB = 500,
+				golonganC = 750,
+				golonganD = 900;
 
 		if (golongan.equalsIgnoreCase("A")) {
 			tunjangan = 0.05 * golonganA;
-			gajiBersih = golonganA + ((0.3 * golonganA)  + tunjangan) + tunjangan;
-		}else if(golongan.equalsIgnoreCase("B")) {
+			gajiBersih = golonganA + ((0.3 * golonganA) + tunjangan) + tunjangan;
+		} else if (golongan.equalsIgnoreCase("B")) {
 			tunjangan = 0.05 * golonganB;
-			gajiBersih = golonganB + ((0.3 * golonganB)  + tunjangan) + tunjangan;
-		}else if(golongan.equalsIgnoreCase("C")) {
+			gajiBersih = golonganB + ((0.3 * golonganB) + tunjangan) + tunjangan;
+		} else if (golongan.equalsIgnoreCase("C")) {
 			tunjangan = 0.05 * golonganC;
-			gajiBersih = golonganC + ((0.3 * golonganC)  + tunjangan) + tunjangan;
-		}else if(golongan.equalsIgnoreCase("D")) {
+			gajiBersih = golonganC + ((0.3 * golonganC) + tunjangan) + tunjangan;
+		} else if (golongan.equalsIgnoreCase("D")) {
 			tunjangan = 0.05 * golonganD;
-			gajiBersih = golonganD + ((0.3 * golonganD)  + tunjangan) + tunjangan;
-		}else {
-			System.out.println("Golongan anda tidak benar");
+			gajiBersih = golonganD + ((0.3 * golonganD) + tunjangan) + tunjangan;
+		} else {
+			System.out.println("Golongan Salah");
 			System.exit(0);
 		}
 
-		System.out.println("Nama : " + name);
-		System.out.println("Golongan : " + golongan);
-		System.out.println("Gaji Bersih : " + gajiBersih);
+		System.out.println("Nama         : " + name + "\n" +
+				"Golongan     : " + golongan + "\n" +
+				"Gaji bersih  : " + gajiBersih);
 	}
 
+	static void soal4a() {
+		/* TERNARY OPERATOR
+			int besar = if (angka1 < angka2) ? angka2 : angka1;
+		*/
+		clearScreen();
+		int angka1, angka2, angka3;
+		System.out.print("Masukkan angka pertama : ");
+		angka1 = scanner();
 
+		System.out.print("Masukkan angka kedua: ");
+		angka2 = scanner();
 
-
-	static void soal4(int angka1, int angka2) {
-		int besar = (angka1 > angka2) ? angka1 : angka2;
-		System.out.println("angka 1 : " + angka1);
-		System.out.println("angka2  : " + angka2);
-		System.out.println("angka terbesar : " + besar);
+		int besar = Math.max(angka1, angka2);
+		System.out.println("Angka 1  : " + angka1 + "\n" +
+				"Angka 2  : " + angka2 + "\n" +
+				"Angka terbesar  : " + besar);
 	}
 
-	static void soal4(int angka1, int angka2, int angka3) {
-		int angkaTerbesar = angka1;
-		int angkaTerkecil = angka1;
+	static void soal4b() {
+		clearScreen();
+		int angka1, angka2, angka3;
 
-		// terkecil
-		if(angka2 < angkaTerkecil) {
-			angkaTerkecil = angka2;
-		}
+		System.out.print("Masukkan angka pertama : ");
+		angka1 = scanner();
 
-		if(angka3 < angkaTerkecil) {
-			angkaTerkecil = angka3;
-		}
-		
-		// terbesar
-		if(angka2 > angkaTerbesar) {
-			angkaTerbesar = angka2;
-		}
+		System.out.print("Masukkan angka kedua   : ");
+		angka2 = scanner();
 
-		if (angka3 > angkaTerbesar){
-			angkaTerbesar = angka3;
-		}
+		System.out.print("Masukkan angka ketiga  : ");
+		angka3 = scanner();
 
-		System.out.println("terkecil : " + angkaTerkecil);
-		System.out.println("terbesar : " + angkaTerbesar);
+		int angkaTerkecil = angka1, angkaTerbesar = angka1;
+
+		// TERKECIL
+		if (angka2 < angkaTerkecil) angkaTerkecil = angka2;
+		if (angka3 < angkaTerkecil) angkaTerkecil = angka3;
+
+		// TERBESAR
+		if (angka2 > angkaTerbesar) angkaTerbesar = angka2;
+		if (angka3 > angkaTerbesar) angkaTerbesar = angka3;
+
+		System.out.println("Angka terkecil : " + angkaTerkecil + "\n" +
+				"Angka terbesar : " + angkaTerbesar);
+	}
+
+	// helpers (overloading scanner)
+	static int scanner() {
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextInt();
+	}
+
+	static String scanner(String message) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print(message + ": ");
+		return scanner.nextLine();
+	}
+
+	static void clearScreen() {
+		try {
+			if (System.getProperty("os.name").contains("Windows")) {
+				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+			} else {
+				System.out.print("\033\143");
+			}
+		} catch (Exception ex) {
+			System.err.println("tidak bisa clear screen");
+		}
 	}
 }
