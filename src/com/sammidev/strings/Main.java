ece1dc117130e56e82d067dbfae57e286d66f9e5
SemmiDev <sammidev4@gmail.com>
2020-12-22 14:52:22 +0700

    string

---
+++ src/com/sammidev/strings/Main.java
@@ -0,0 +1,36 @@
+package com.sammidev.strings;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // StringBuffer
+        var name = new StringBuffer("Sammi");
+        name.append(" Aldhi Yanto");
+        System.out.println(name); // Sammi Aldhi Yanto
+
+        // reverse()
+        var reverse = name.reverse();
+        System.out.println(reverse); // otnaY ihdlA immaS
+
+        // delete()
+        name.reverse();
+        var delete = name.delete(3,7); // index 3 sampai index 6 (offset)
+        System.out.println(delete); // Samldhi Yanto
+
+        // insert()
+        var insert = name.insert(2, "INSERT"); // start from index 2
+        System.out.println(insert); // SaINSERTmldhi Yanto
+
+        // replace()
+        var replace = name.replace(3,7,"REPLACE"); // start from index 3-6
+        System.out.println(replace); // SaIREPLACETmldhi Yanto
+
+        // length()
+        var length = name.length();
+        System.out.println(length); // 22
+
+        // concat()
+        var concat = name + " ganteng" + " hahaha";
+        System.out.println(concat); // SaIREPLACETmldhi Yanto ganteng hahaha
+    }
+}
