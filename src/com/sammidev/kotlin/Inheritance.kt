ef1bf6703525ff803b1179d66ad58fc545e44870
SemmiDev <sammidev4@gmail.com>
2021-01-15 09:12:55 +0700

    child constructor

--- src/com/sammidev/kotlin/Inheritance.kt
+++ src/com/sammidev/kotlin/Inheritance.kt
@@ -38,13 +38,36 @@ class Siswaa(name: String) : Siswa(name) {
 
 }
 
+
+
+open class Mahasiswa(val name: String, val age: Int, val uktLevel: Int) {
+    constructor(name: String,age: Int) : this(name,age,1)
+    constructor(age: Int, uktLevel: Int) : this("", age,uktLevel)
+
+    fun hello() {
+        println("${this.name} ${this.age} ${this.uktLevel}")
+    }
+}
+
+class Sammidev : Mahasiswa {
+    constructor() : super("sammidev",12,2)
+    constructor(name: String) : super(name,12,2)
+    constructor(age: Int, uktLevel: Int) : super("", age, uktLevel)
+
+    fun show() {
+        println(super.name + super.age + super.uktLevel)
+    }
+}
+
 fun main() {
+
+    val sam = Sammidev(2,10)
+    sam.show()
+
 
     val sammi = Siswaa("Sammidev")
     sammi.hello()
     sammi.superr()
-
-
 
     val manager: Person = Manager("sam")
     manager.fullname("sammidev")
